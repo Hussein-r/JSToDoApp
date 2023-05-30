42841592e216e35cc6fc8dc00332a3410ea66e9c
hussein-r <hussein.rk94@gmail.com>
2023-05-31 00:50:47 +0300

    Edit feature

--- script.js
+++ script.js
@@ -20,13 +20,25 @@ class Task {
 
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("#btn").addEventListener("click", () => {
-    let task = new Task(
-      tasks.length + 1,
-      document.querySelector("#task").value,
-      document.querySelector("#priority").value
-    );
-    task.addTask();
-    ctrlLayout();
+    let taskName = document.querySelector("#task").value;
+    let taskPriority = document.querySelector("#priority").value;
+    if (validateTask(taskName, taskPriority)) {
+      if (document.querySelector("#btn").innerHTML === "Update") {
+        const task = tasks.find(
+          (element) => element.id == document.querySelector("#id").value
+        );
+        task.task = taskName;
+        task.priority = taskPriority;
+        document.querySelector("#btn").innerHTML = "Add";
+        ctrlLayout();
+      } else {
+        const task = new Task(tasks.length + 1, taskName, taskPriority);
+        task.addTask();
+        ctrlLayout();
+      }
+    } else {
+      alert("Enter Valid Task name and priority");
+    }
   });
   ctrlLayout();
 });
@@ -36,7 +48,7 @@ const loadTasks = function () {
   tasks
     .sort((t1, t2) => priority[t1.priority] - priority[t2.priority])
     .forEach((task) => {
-      tasksTable += `<tr><td>${task.id}</td><td>${task.task}</td><td>${task.priority}</td><td><button class="delbtn" onclick="removeTask(${task.id})">Remove</button></td></tr>`;
+      tasksTable += `<tr><td>${task.id}</td><td>${task.task}</td><td>${task.priority}</td><td><button class="editbtn" onclick="editTask(${task.id})">Edit</button></td><td><button class="delbtn" onclick="removeTask(${task.id})">Remove</button></td></tr>`;
     });
   document.querySelector("tbody").innerHTML = tasksTable;
 };
@@ -56,4 +68,16 @@ const removeTask = function (id) {
   const task = tasks.find((element) => element.id == id);
   task.removeTask();
   ctrlLayout();
+};
+
+const editTask = function (id) {
+  const task = tasks.find((element) => element.id == id);
+  document.querySelector("#btn").innerHTML = "Update";
+  document.querySelector("#id").value = id;
+  document.querySelector("#task").value = task.task;
+  document.querySelector("#priority").value = task.priority;
+};
+
+const validateTask = function (taskName, taskPriority) {
+  return taskName && ["low", "medium", "high"].includes(taskPriority);
 };
